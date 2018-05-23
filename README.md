26445968d31c568948de0af5c35a2779142d77b8
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-05-23 14:14:53 +0200

    forgot to add files

---
+++ README.md
@@ -0,0 +1,1 @@
+BCO Cloud Server
---
+++ app.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.send('BCO Cloud prototype');
+});
+
+const pwd = "123456";
+
+io.on('connection', function (socket) {
+    console.log('a user connected with id: ' + socket.id);
+
+    // initialize a timeout of 3 seconds to close the socket connection if no authentication has been performed
+    let authenticationTimeout = setTimeout(() => socket.disconnect(true), 3000);
+
+    socket.on('authenticate', function (data) {
+        console.log('authenticate with data: ' + data);
+
+        let callback = arguments[arguments.length - 1];
+        if (data === pwd) {
+            clearTimeout(authenticationTimeout);
+            callback('Authentication complete!');
+        } else {
+            callback('Authentication failed!');
+            socket.disconnect(true);
+            clearTimeout(authenticationTimeout);
+        }
+    });
+
+    socket.on('disconnect', () => console.log('socket[' + socket.id + '] disconnected'));
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
