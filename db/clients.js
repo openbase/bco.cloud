516f4b30f1660a2daa230a75d0930033dbd268c5
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-05-28 14:52:37 +0200

    adapt redirect URI for testing

--- db/clients.js
+++ db/clients.js
@@ -2,7 +2,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_ID || 'google';
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET || 'googleSecret';
 
 const clients = [
-    {id: GOOGLE_CLIENT_ID, redirectURI: "http://localhost:5000", secret: GOOGLE_SECRET}
+    {id: GOOGLE_CLIENT_ID, redirectURI: "https%3A%2F%2Fdevelopers.google.com%2Foauthplayground", secret: GOOGLE_SECRET}
 ];
 
 module.exports.findById = function (id, done) {
