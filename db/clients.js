37939bc8a2466f32daaed859f0e2a8d9ff64de70
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-05-28 15:19:30 +0200

    adapt redirect uri to google project

--- db/clients.js
+++ db/clients.js
@@ -2,7 +2,7 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_ID || 'google';
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET || 'googleSecret';
 
 const clients = [
-    {id: GOOGLE_CLIENT_ID, redirectURI: "https://developers.google.com/oauthplayground", secret: GOOGLE_SECRET}
+    {id: GOOGLE_CLIENT_ID, redirectURI: "https://oauth-redirect.googleusercontent.com/r/testsmarthome-6203c", secret: GOOGLE_SECRET}
 ];
 
 module.exports.findById = function (id, done) {
