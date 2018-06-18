047d2c5cf8682f03017eb9e51618cfe436c4347d
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-06-18 10:28:11 +0200

    added some debug messages

--- db/clients.js
+++ db/clients.js
@@ -1,6 +1,9 @@
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_ID || 'google';
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET || 'googleSecret';
 
+console.log("GoogleClientID: " + GOOGLE_CLIENT_ID);
+console.log("GoogleSecret: " + GOOGLE_SECRET);
+
 const clients = [
     {id: GOOGLE_CLIENT_ID, redirectURI: "https://oauth-redirect.googleusercontent.com/r/bcosmarthomeaction", secret: GOOGLE_SECRET}
 ];
--- db/users.js
+++ db/users.js
@@ -1,6 +1,9 @@
 const TEST_USER = process.env.TEST_USER || 'bco';
 const TEST_PASSWORD = process.env.TEST_PASSWORD || 'pwd';
 
+console.log("TestUser: " + TEST_USER);
+console.log("TestPassword: " + TEST_PASSWORD);
+
 const users = [
     {username: TEST_USER, password: TEST_PASSWORD}
 ];
@@ -10,6 +13,7 @@ module.exports.findByUsername = function (username, done) {
         if (users[i].username === username) {
             return done(null, users[i]);
         }
+        console.log("Could not find user[" + username + "]")
         return done(new Error("User not found"));
     }
 };
